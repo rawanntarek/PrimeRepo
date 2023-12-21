21c6b4fedff57f2274db798e30bdf3a4913bb201
rawanntarek <rawaaan245@gmail.com>
2023-12-21 18:43:51 +0200

    initial version

---
+++ SocialNW/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+ class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        boolean isPrime = checkPrime(number);
+
+        // Display the result
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    private static boolean checkPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
